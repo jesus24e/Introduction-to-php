ced6c440c7ae97ccac7818882b3a2ccadf9a440b
jesus24e <jesusrodriguez65@aragon.unam.mx>
2026-08-10 22:33:03 -0600

    manejo de archivos en php

---
+++ formularios_php/manejo_de_archivos.php
@@ -0,0 +1,26 @@
+<?php
+
+    $miarchivo = fopen("miarchivo.txt","r") or die("no se puede abrir el archivo");
+
+    echo fread($miarchivo, filesize("miarchivo.txt"));
+
+    /*
+    Parámetros del fopen()
+
+    "r" - Read only (Sólo lectura). Comienza al inicio del archivo
+    "r+" - Read/Write (Lectura/escritura). Comienza al inicio del archivo
+    "w" - Write only (sólo escritura). Abre y trunca o vacía el archivo. Desplaza el puntero al inicio del archivo
+    "w+" - Read/Write (lectura escritura). Abre y vacía el archivo o crea un archivo que no existe. Mueve el puntero al inicio del archivo
+    "a" - Write only. (sólo escritura) Abre y escribe al final del archivo o crea uno nuevo si este no existe
+    "a+" - Read/Write (lectura/escritura). Preserva el contenido del archivo escribiendo al final de su contenido
+    "x" - Write only. (sólo escritura) Crea un nuevo archivo y devuelve un error y un FALSE si el archivo ya existe
+    "x+" - Read/Write. (lectura/escritura) Crea un nuevo archivo o devuelve un error y un FALSE si el archivo ya existe
+    "c" - Write only. (sólo escritura) Abre el archivo o crea uno nuevo si este ya existe. Mueve el puntero al inicio del archivo
+    "c+" - Read/Write. (lectura/escritura) Abre el archivo o crea uno si este no existe. Desplaza el puntero al inicio del archivo
+
+    */
+
+    fclose($miarchivo);
+    
+
+?>
