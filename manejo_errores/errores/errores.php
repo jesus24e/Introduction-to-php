b0e7078855540c299c877c0c87536bb3a7d93b69
jesus24e <jesusrodriguez65@aragon.unam.mx>
2026-08-14 18:58:57 -0600

    manejo de errores con try catch, tipos de errores y finally

--- manejo_errores/errores/errores.php
+++ manejo_errores/errores/errores.php
@@ -3,7 +3,7 @@
     ini_set('display_errors', 1);
 
     //? NOTICE — variable no definida
-    echo $variableInexistente;
+    //echo $variableInexistente;
 
     //? WARNING — archivo que no existe (el script continúa)
     include("no_existe.php");
@@ -12,12 +12,31 @@
     try {
         throw new Exception("esto es una excepción");
     } catch (Exception $e) {
-        echo "Excepción atrapada: " . $e->getMessage();
+        echo "Excepción atrapada: " . $e->getMessage()."\n";
     }
+    finally{
+        echo "se termina el try catch\n";//!siempre se ejecuta despues de que hay un error, normalmente es cerrar recursos como conexiones a bases de datos
+    }
+    
+    class DBexception extends Exception{}
+    
+    class Main{
+        public function __construct()
+        {
+            try{
+                throw new DBexception("error con la conexion a la db.");
+            }catch(DBexception $e){
+                echo "-------------error especifico: ".$e->getMessage()."-----------------------\n";
+            }
+        }
+    }
+    
+    $main = new Main();
+
+    
 
     //? EXCEPCIÓN SIN ATRAPAR — mata el script
     throw new Exception("excepción sin atrapar");
 
     //? FATAL ERROR — función que no existe (mata el script)
     // funcionQueNoExiste();
-?>
